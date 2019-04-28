35f95f09f6594da0b14fc21b2a6a61177abc36de
MAKSYM_enko <finside41@gmail.com>
2019-04-28 16:20:21 +0300

    Updates

---
+++ precache-manifest.c4463df19e257cf2b0270454eb0f3c51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac264675002e0302925b",
+    "url": "/movie-app-react/static/js/main.4b7f3ebf.chunk.js"
+  },
+  {
+    "revision": "3dc31cdd2528aca2ddc9",
+    "url": "/movie-app-react/static/js/runtime~main.11787b41.js"
+  },
+  {
+    "revision": "6a5dd8352e234a6ebe05",
+    "url": "/movie-app-react/static/css/2.64707eff.chunk.css"
+  },
+  {
+    "revision": "6a5dd8352e234a6ebe05",
+    "url": "/movie-app-react/static/js/2.44c7a01c.chunk.js"
+  },
+  {
+    "revision": "af9c34699a9715bfbefdcc0927843f68",
+    "url": "/movie-app-react/index.html"
+  }
+];
